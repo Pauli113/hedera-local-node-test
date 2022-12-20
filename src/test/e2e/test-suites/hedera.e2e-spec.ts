f8f309e6480cc551d4a995a6f7d160a289e6d97c
Pauli113 <35477509+Pauli113@users.noreply.github.com>
2022-12-20 14:37:52 +0800

    import fixed

--- src/test/e2e/test-suites/hedera.e2e-spec.ts
+++ src/test/e2e/test-suites/hedera.e2e-spec.ts
@@ -1,12 +1,12 @@
 //import { curl } from "../lib/curl";
 //let request = require('request')
+//let curl = require('../lib/curl');
 
 
 describe('hedera localnode', () => {
   it('checks if node is running', async()=> {
     console.log('test running')
     console.warn('http://localhost:9090/testnet/dashboard?p2=1&k2=1671514013.107249003&p1=1&k1=1671514017.885134315&p3=1&k3=1671513818.425212003')
-
+    
   })
-  
 });
