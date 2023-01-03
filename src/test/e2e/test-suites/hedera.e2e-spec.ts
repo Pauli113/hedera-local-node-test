baf894ce35eebc8915332e105be50a2595a9189d
Pauli113 <35477509+Pauli113@users.noreply.github.com>
2023-01-03 13:45:12 +0800

    signed and unsigned transaction running

--- src/test/e2e/test-suites/hedera.e2e-spec.ts
+++ src/test/e2e/test-suites/hedera.e2e-spec.ts
@@ -67,10 +67,8 @@ describe('hedera localnode', () => {
       throw new Error("Environment variables myAccountId and myPrivateKey must be present");
   }
 
-  // Create our connection to the Hedera network
-  // The Hedera JS SDK makes this really easy!
-  const client = Client.forTestnet();
-
+  //need to switch to localnet
+  const client = Client.forTestnet()
   client.setOperator(myAccountId, myPrivateKey);
 
   //Create new keys
@@ -103,8 +101,21 @@ describe('hedera localnode', () => {
   it('should create a signed transaction', async() => {
     const myAccountId = process.env.MY_ACCOUNT_ID;
     const myPrivateKey = process.env.MY_PRIVATE_KEY;
-   
+    const newAccountPrivateKey = PrivateKey.generateED25519(); 
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
 
+    const node = {"127.0.0.1:50211": new AccountId(3)};
+    const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
+    client.setOperator(myAccountId, myPrivateKey);
+   
+    const transaction = await new AccountUpdateTransaction()
+    .setAccountId(myAccountId)
+    .setKey(newAccountPublicKey)
+    .freezeWith(client)
+
+    //Sign the transaction with a private key
+    const signedTransaction = transaction.sign(PrivateKey.fromString(myPrivateKey))
+    console.warn(signedTransaction)
   })
 
   it('should create an unsigned transaction', async()=> {
@@ -113,11 +124,10 @@ describe('hedera localnode', () => {
     const newAccountPrivateKey = PrivateKey.generateED25519(); 
     const newAccountPublicKey = newAccountPrivateKey.publicKey;
 
-    const client = Client.forTestnet();
+    const node = {"127.0.0.1:50211": new AccountId(3)};
+    const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
 
     client.setOperator(myAccountId, myPrivateKey);
-
-    
 
     const transaction = new AccountCreateTransaction()
     .setKey(newAccountPublicKey)
