963b800ad45783f930ceace08803e7162144e657
Pauli113 <35477509+Pauli113@users.noreply.github.com>
2023-01-10 10:39:23 +0800

    fixed main

--- src/test/e2e/test-suites/hedera.e2e-spec.ts
+++ src/test/e2e/test-suites/hedera.e2e-spec.ts
@@ -15,6 +15,8 @@ const {
 require("dotenv").config();
 
 describe('hedera localnode', () => {
+    jest.setTimeout(1250000)
+
 /*
 
   it('checks if node is running', async()=> {
@@ -59,8 +61,8 @@ describe('hedera localnode', () => {
   })
 
   it('should create an account', async() => {
-    const myAccountId = process.env.MY_ACCOUNT_ID;
-    const myPrivateKey = process.env.MY_PRIVATE_KEY;
+    const myAccountId = '0.0.1032';
+    const myPrivateKey = 'ab2ca606fb4a844c5fb6c64f747de3bfd763aff285409d97d197661c78d1316e';
     // If we weren't able to grab it, we should throw a new error
     if (myAccountId == null ||
       myPrivateKey == null ) {
@@ -68,34 +70,27 @@ describe('hedera localnode', () => {
   }
 
   //need to switch to localnet
-  const client = Client.forTestnet()
-  client.setOperator(myAccountId, myPrivateKey);
+  //const client = Client.forTestnet()
+  //const node = {"127.0.0.1:50211": new AccountId(3)};
+  //const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
+  const node = {"127.0.0.1:50211": new AccountId(3)};
+  const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
 
-  //Create new keys
   const newAccountPrivateKey = PrivateKey.generateED25519(); 
   const newAccountPublicKey = newAccountPrivateKey.publicKey;
-  console.warn(String(newAccountPrivateKey))
-  console.warn(String(newAccountPublicKey))
-
-
+  
   //Create a new account with 1,000 tinybar starting balance
-  const newAccount = await new AccountCreateTransaction()
-      .setKey(newAccountPublicKey)
-      .setInitialBalance(Hbar.fromTinybars(1000))
-      .execute(client);
+  const newAccount = new AccountCreateTransaction()
+  .setKey(myPrivateKey)
+  .setInitialBalance(Hbar.fromTinybars(1000));
 
   // Get the new account ID
-  const getReceipt = await newAccount.getReceipt(client);
-  const newAccountId = getReceipt.accountId;
+//const getReceipt = await newAccount.getReceipt(client);
+//const newAccountId = getReceipt.accountId;
 
-  console.log("The new account ID is: " +newAccountId);
+//Log the account ID
+//console.log("The new account ID is: " +newAccountId);
 
-  //Verify the account balance
-  const accountBalance = await new AccountBalanceQuery()
-      .setAccountId(newAccountId)
-      .execute(client);
-
-  console.log("The new account balance is: " + await accountBalance.hbars.toTinybars() +" tinybar.");
   })
 
   it('should create a signed transaction', async() => {
@@ -114,11 +109,38 @@ describe('hedera localnode', () => {
     .freezeWith(client)
 
     //Sign the transaction with a private key
-    const signedTransaction = transaction.sign(PrivateKey.fromString(myPrivateKey))
-    jest.setTimeout(30000)
+    const signedTransaction = await transaction.sign(PrivateKey.fromString(myPrivateKey))
     console.warn(signedTransaction)
   })
+/*
+  it('should create an unsigned transaction', async()=> {
 
+    const myAccountId = process.env.MY_ACCOUNT_ID;
+    const myPrivateKey = process.env.MY_PRIVATE_KEY;
+    const newAccountPrivateKey = PrivateKey.generateED25519(); 
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
+
+    const node = {"127.0.0.1:50211": new AccountId(3)};
+    const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
+
+    client.setOperator(myAccountId, myPrivateKey);
+
+    const transaction = new AccountCreateTransaction()
+    .setKey(newAccountPublicKey)
+    .setInitialBalance(Hbar.fromTinybars(1000));
+
+    //Freeze the transaction for signing
+    //The transaction cannot be modified after this point
+
+    const freezeTransaction = await transaction.freezeWith(client);
+
+    const txResponse = await freezeTransaction.execute(client);
+
+    const receipt = await txResponse.getReceipt(client);
+    const newAccountId = receipt.accountId;
+    console.log("The new account ID is " +newAccountId);
+  })*/
+  
   it('should create an unsigned transaction', async()=> {
     const myAccountId = process.env.MY_ACCOUNT_ID;
     const myPrivateKey = process.env.MY_PRIVATE_KEY;
@@ -136,8 +158,83 @@ describe('hedera localnode', () => {
 
     //Freeze the transaction for signing
     //The transaction cannot be modified after this point
-    const freezeTransaction = await transaction.freezeWith(client);
+    const freezeTransaction = transaction.freezeWith(client);
 
-    console.warn(freezeTransaction);
   })
-});
+
+  it('paul localnet - unsigned', async() => {
+    const ACCOUNT_ID = '0.0.1032';
+    const BASE_KEY = 'ab2ca606fb4a844c5fb6c64f747de3bfd763aff285409d97d197661c78d1316e';
+
+    const node = {"127.0.0.1:50211": new AccountId(3)};
+    const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
+    const privateKey = PrivateKey.fromStringECDSA(BASE_KEY)
+    const publicKey = privateKey.publicKey;
+
+    client.setOperator(ACCOUNT_ID, privateKey);
+
+    const transaction = new AccountCreateTransaction()
+    .setKey(publicKey)
+    .setInitialBalance(Hbar.fromTinybars(1000));
+
+    const freezeTransaction = transaction.freezeWith(client);
+    console.warn(freezeTransaction)
+
+  })
+
+  it('paul localnet - signed', async() => {
+    const myAccountId = '0.0.1032';
+    const myPrivateKey = 'ab2ca606fb4a844c5fb6c64f747de3bfd763aff285409d97d197661c78d1316e';
+    const newAccountPrivateKey = PrivateKey.generateED25519(); 
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
+
+    const node = {"127.0.0.1:50211": new AccountId(3)};
+    const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
+    client.setOperator(myAccountId, myPrivateKey);
+   
+    const transaction = await new AccountUpdateTransaction()
+    .setAccountId(myAccountId)
+    .setKey(newAccountPublicKey)
+    .freezeWith(client)
+    //const txResponse = transaction.execute(client);
+
+    const signedTransaction = await transaction.sign(PrivateKey.fromString(myPrivateKey))
+    console.warn(signedTransaction)
+  })
+
+  it('paul localnet - signed2', async() => {
+    const myAccountId = '0.0.1032';
+    const myPrivateKey = 'ab2ca606fb4a844c5fb6c64f747de3bfd763aff285409d97d197661c78d1316e';
+    const newAccountPrivateKey = PrivateKey.generateED25519(); 
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
+
+    const node = {"127.0.0.1:50211": new AccountId(3)};
+    const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
+    client.setOperator(myAccountId, myPrivateKey);
+   
+    const transaction = await new AccountUpdateTransaction()
+    .setAccountId(myAccountId)
+    .setKey(newAccountPublicKey)
+    .freezeWith(client)
+    //const txResponse = transaction.execute(client);
+
+    const signedTransaction = await transaction.sign(PrivateKey.fromString(myPrivateKey))
+    console.warn(signedTransaction)
+  })
+
+  it('submits a transaction', async() => {
+    const myAccountId = '0.0.1032';
+    const myPrivateKey = 'ab2ca606fb4a844c5fb6c64f747de3bfd763aff285409d97d197661c78d1316e';
+    const newAccountPrivateKey = PrivateKey.generateED25519(); 
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
+    const node = {"127.0.0.1:50211": new AccountId(3)};
+    const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
+    client.setOperator(myAccountId, myPrivateKey);
+    //Submit a transaction to your local node
+    const transaction = await new AccountUpdateTransaction()
+    .setAccountId(myAccountId)
+    .setKey(newAccountPublicKey)
+    .freezeWith(client)
+  })
+
+})
